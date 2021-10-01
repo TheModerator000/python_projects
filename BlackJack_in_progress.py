89c39c384c76008dc18e272cf71505341266f4f8
Jbrose1 <89755424+Jbrose1@users.noreply.github.com>
2021-10-01 14:52:49 -0500

    Update BlackJack_in_progress.py

--- BlackJack_in_progress.py
+++ BlackJack_in_progress.py
@@ -1,4 +1,10 @@
+# Created by: Justin Brose
+# Date: 10/1/21
+
 import random
+
+# Directions for play
+
 print("Welcome to Black Jack!")
 print("Here is how this version works: ")
 print("1.) The main objective is to get the dealer's bank to 0. You can do this by beating the dealer to 21 or not 'busting'. If you 'bust' or the dealer 'bust' then credits are lost.")
@@ -12,15 +18,20 @@ print("8.) If you choose to stay and your value is closer to 21 than the dealer'
 print("That is all the rules for this game of Black Jack. Have Fun!")
 
 # first we need to give the code our values for cards
+
 card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 bank = 500
 dealer_bank = 500
+
 # next you have to define your first card's value. In this case, your first cards
-# is a random number from our card values list
+# are a random number from our card values list.
+
 first_card = random.choice(card_values)
 dealer_first_card = random.choice(card_values)
 
 play_again = 'yes'
+
+# Allows for the game to start again
 
 while play_again == 'yes':
   if bank <= 0:
@@ -38,6 +49,9 @@ while play_again == 'yes':
   turn = "hit"
   dealer_turn = "hit"
 
+# functionality for 'hit', player, and dealer
+# the bank and dealer_bank arguments allow for funds to inc and dec
+# from their variables.
 
   while turn == "hit":
       sum += random.choice(card_values)
@@ -45,8 +59,16 @@ while play_again == 'yes':
       print("You currently have: ", sum)
       print("The dealer's cards are face down.")
 
+# the code below allows for the program to check if the bank is empty. If the
+# bank is empty, then the program will close out with the message
+# 'Better luck next time!'
+
       if bank <= 0:
           print("Better luck next time!") + exit()
+
+# Below code displays all scenarios in the game as if functions. The program will
+# evaluate each one to see if they are true and if they are then it will execute
+# the assigned argument.
 
       elif sum < 21 and dealer_sum > 21:
           print("The dealer bust with: ",dealer_sum, "! Here is your reward", bet * 1.25)
@@ -88,6 +110,8 @@ while play_again == 'yes':
           bank -= bet
           dealer_bank += bet * 2
 
+# The code below checks to see if the round is over and asks if the player wants
+# to try again.
 
       if sum < 21 and dealer_sum > 21:
           play_again = input("Want to play_again?")
@@ -96,6 +120,10 @@ while play_again == 'yes':
           play_again = input("Want to play again?")
           break
 
+# The code below checks to see if the round is still going. If it is the if
+# statement gives the player the option to either say 'hit' or 'stay'. If then
+# player says stay then the code will check if who is closer to 21. The play That
+# is closer to 21 will be awarded points.
       if sum < 21:
           turn = input("what do you want to do?")
           if turn == "stay":
@@ -118,6 +146,9 @@ while play_again == 'yes':
                   dealer_bank += bet * 2
       else:
           turn = "bust"
+
+# This last bit of code checks if the round is complete. If so, the player can
+# type in 'yes' and a new round will start.
       if sum > 21:
           play_again = input("Want to play again?")
           break
